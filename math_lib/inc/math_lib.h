e785db08000b79784a4d3b0812c9c4fe3771fa92
centurion0929 <sergiy_pukalyak@ukr.net>
2024-01-15 10:08:25 +0200

    Add the fibonacci series function
    Signed-off-by: centurion0929 <sergiy_pukalyak@ukr.net>

--- math_lib/inc/math_lib.h
+++ math_lib/inc/math_lib.h
@@ -1,1 +1,2 @@
 int multiply_int(int a, int b);
+unsigned fibonacci_series(unsigned number);
--- math_lib/src/math_lib.c
+++ math_lib/src/math_lib.c
@@ -4,3 +4,11 @@ int multiply_int(int a, int b)
 {
 	return a * b;
 }
+
+unsigned fibonacci_series(unsigned number)
+{
+    if (number <= 1)
+        return number;
+
+    return fibonacci_series(number - 1) + fibonacci_series(number - 2);
+}
