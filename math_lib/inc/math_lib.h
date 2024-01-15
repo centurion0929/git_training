c242358dbe9ea8a46d3d4f2bc848d05e063b1287
centurion0929 <sergiy_pukalyak@ukr.net>
2024-01-15 09:47:31 +0200

    Add the factorial function
    Signed-off-by: centurion0929 <sergiy_pukalyak@ukr.net>

--- math_lib/inc/math_lib.h
+++ math_lib/inc/math_lib.h
@@ -1,1 +1,2 @@
 int sum_int(int a, int b);
+unsigned factorial(unsigned number);
--- math_lib/src/math_lib.c
+++ math_lib/src/math_lib.c
@@ -4,3 +4,11 @@ int sum_int(int a, int b)
 {
 	return a + b;
 }
+
+unsigned factorial(unsigned number)
+{
+	if (number == 0 || number == 1)
+		return 1;
+
+	return number * factorial(number - 1);
+}
